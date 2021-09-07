1e8102e11c403a2a11699102e6327057b7ddcd68
Douglas-CRVG <douglas.07.nascimento@gmail.com>
2021-09-07 18:56:28 -0300

    Routes created

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,29 @@
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Login from './Login/Login';
+import SignUp from './SignUp/SignUp';
+import Today from './Today/Today';
+import History from './History/History';
+import Habits from './Habits/Habits';
+
 export default function App() {
     return (
         <BrowserRouter>
-            <header>Tô aqui</header>
             <Switch>
-
+                <Route path="/" exact>
+                    <Login />
+                </Route>
+                <Route path="/cadastro" exact>
+                    <SignUp />
+                </Route>
+                <Route path="/habitos" exact>
+                    <Habits />
+                </Route>
+                <Route path="/hoje" exact>
+                    <Today />
+                </Route>
+                <Route path="/historico" exact>
+                    <History />
+                </Route>
             </Switch>
         </BrowserRouter>
     );
---
+++ src/components/Habits/Habits.js
@@ -0,0 +1,5 @@
+export default function Habits (){
+    return (
+        <h1>Habits existe</h1>
+    );
+}
